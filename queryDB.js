53c2bcba333435693271f75743a1b5ebe2363a55
Javier 'Javi' Carrion <javier.carrion@designinteractive.net>
2023-02-14 18:23:01 -0500

    feat: :sparkles: add query data and retrieve data

---
+++ queryDB.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import { exit } from 'process';
+
+export default async function queryDB(externalFunction) {
+    try {
+        let info = [];
+        if (fs.existsSync('db.json')) {
+            await fs.readFile('db.json', (error, data) => {
+                if (error) {
+                    console.log('Reading file has failed!', error);
+                    return;
+                }
+
+                info = JSON.parse(data.toString());
+                console.log(info);
+
+                if (externalFunction && !error) {
+                    externalFunction(info);
+                    return;
+                }
+            });
+        } else {
+            if (externalFunction) {
+                externalFunction(info);
+                return;
+            }
+        }
+    } catch (error) {
+        console.log('Something went wrong', error);
+    }
+}
