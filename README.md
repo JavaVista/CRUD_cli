3ac28aa2da8975d250a1b5b9672c5d580f3c3764
Javier 'Javi' Carrion <javier.carrion@designinteractive.net>
2023-02-21 22:18:43 -0500

    feat: :sparkles: add data and create file

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# CRUD Command Line App with Nodejs 
+# CRUD Command Line App with Nodejs
 
 No external Database
 
-## Dependecies
+## Dependencies
 
 * inquirer - provides an easy way to capture user input in your Node. js command line interface
-* uuid - a secure way to generate cryptographically strong unique identifiers
+* uuid - a secure way to generate cryptographically strong unique identifier
---
+++ addData.js
@@ -0,0 +1,63 @@
+import inquirer from 'inquirer';
+import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
+import queryDB from './queryDB.js';
+
+export default async function addData(info) {
+    try {
+        const answer = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Please, enter your name:',
+            },
+            {
+                type: 'number',
+                name: 'phone',
+                message: 'Please, enter your phone number:',
+            },
+            {
+                type: 'list',
+                name: 'age',
+                message: 'Are you an Adult?',
+                choices: [
+                    { name: 'Y', value: 'Adult' },
+                    { name: 'N', value: 'Minor' },
+                ],
+            },
+        ]);
+
+        const data = {
+            id: uuidv4(),
+            name: answer.name,
+            phoneNumber: answer.phone,
+            adult: answer.age,
+        };
+        info.push(data);
+
+        if (fs.existsSync('db.json')) {
+            addDetails(info);
+        } else {
+            fs.appendFile('db.json', '[]', function (error) {
+                if (error) {
+                    console.log('Creating file failed.');
+                }
+            });
+            console.log('db.json file was successfully created!');
+            addDetails(info);
+        }
+    } catch (error) {
+        console.log('Something went wrong', error);
+    }
+}
+
+async function addDetails(info) {
+    await fs.writeFile('db.json', JSON.stringify(info), function (error) {
+        if (error) {
+            console.log('Er`ror writing to the database file');
+        }
+        console.log('Data was successfully added!');
+    });
+}
+
+queryDB(addData);
